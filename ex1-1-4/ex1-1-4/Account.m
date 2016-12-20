0920557382b477fba738fa8e8f79122a882f404b
RyotaFukumoto <k014c1106@it-neec.jp>
2016-12-21 00:42:06 +0900

    Fixed. #4の課題が完了しました。

---
+++ ex1-1-4/ex1-1-4/Account.m
@@ -0,0 +1,31 @@
+//
+//  Account.m
+//  ex1-1-4
+//
+//  Created by ryota on 2016/12/20.
+//  Copyright © 2016年 ryota. All rights reserved.
+//
+
+#import "Account.h"
+@implementation Account
+
+
+-(id)initWithName:(NSString *) name :(NSInteger) age :(NSString *) sex :(NSString *)language{
+    if (self = [super init]) {
+        self.name = name;
+        self.age = age;
+        self.sex = sex;
+        self.language = language;
+    }
+    return self;
+}
+
+-(void)print{
+    if ([_sex  isEqual: @"男性"]) {
+        NSLog(@"%@君は、%@が得意な%zd歳です。", _name, _language, _age);
+    }else{
+        NSLog(@"%@さんは、%@が得意な%zd歳です。", _name, _language, _age);
+    }
+}
+
+@end
