1497d02d6f095bdbdd16869a960b2925709657a6
RyotaFukumoto <k014c1106@it-neec.jp>
2016-12-23 03:22:37 +0900

    Fixed. #7の課題が完了しました。

---
+++ ex2-1-2/ex2-1-2/ex2-1-2/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  ex2-1-2
+//
+//  Created by ryota on 2016/12/23.
+//  Copyright © 2016年 ryota. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *PoohImageView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    _PoohImageView.contentMode =  UIViewContentModeScaleAspectFit;
+    _PoohImageView.clipsToBounds=YES;
+    _PoohImageView.layer.cornerRadius=100;
+    UIImage *img = [UIImage imageNamed:@"pooh_ex_02.jpg"];
+    _PoohImageView.image = img;
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
