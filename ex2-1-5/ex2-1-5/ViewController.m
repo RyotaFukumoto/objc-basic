b83e722245a0206307785710f1eeee4061f4264d
RyotaFukumoto <k014c1106@it-neec.jp>
2016-12-23 11:56:19 +0900

    Fixed. #10の課題が完了しました。

---
+++ ex2-1-5/ex2-1-5/ViewController.m
@@ -0,0 +1,65 @@
+//
+//  ViewController.m
+//  ex2-1-5
+//
+//  Created by ryota on 2016/12/23.
+//  Copyright © 2016年 ryota. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+- (IBAction)ActionSheetBTN:(id)sender {
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"シェア"
+                                                                             message:@""
+            preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    [alertController addAction:[UIAlertAction actionWithTitle:@"Facebook"
+                                                        style:UIAlertActionStyleDefault
+                                                      handler:^(UIAlertAction *action) {
+                                                          [self Facebook];
+                                                      }]];
+    
+    [alertController addAction:[UIAlertAction actionWithTitle:@"Twitter"
+                                                        style:UIAlertActionStyleDefault
+                                                      handler:^(UIAlertAction *action) {
+                                                          // cancelボタンが押された時の処理
+                                                          
+                                                          [self Twitter];
+                                                      }]];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"Line"
+                                                        style:UIAlertActionStyleDefault
+                                                      handler:^(UIAlertAction *action) {
+                                                          // cancelボタンが押された時の処理
+                                                          
+                                                          [self Line];
+                                                      }]];
+    
+    [self presentViewController:alertController animated:YES completion:nil];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+-(void)Facebook{
+    NSLog(@"Facebook");
+}
+-(void)Twitter{
+    NSLog(@"Twitter");
+}
+-(void)Line{
+    NSLog(@"Line");
+}
+
+@end
