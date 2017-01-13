bf5dead4dfe1a176e86ddb3800617efec28507ec
RyotaFukumoto <k014c1106@it-neec.jp>
2017-01-13 14:05:25 +0900

    Fixed. #19の課題が完了しました。

---
+++ ex2-1-14/ex2-1-14/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  ex2-1-14
+//
+//  Created by ryota on 2017/01/13.
+//  Copyright © 2017年 ryota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController<UITableViewDelegate,UITableViewDataSource>
+
+
+
+@end
+
