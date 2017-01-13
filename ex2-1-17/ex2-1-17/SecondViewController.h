6c283330835ea1b67e21b5586814ccb7f8962acb
RyotaFukumoto <k014c1106@it-neec.jp>
2017-01-13 20:39:12 +0900

    Fixed. #22の課題が完了しました。

---
+++ ex2-1-17/ex2-1-17/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  ex2-1-17
+//
+//  Created by ryota on 2017/01/13.
+//  Copyright © 2017年 ryota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
