fd4bfb43779fb6ed02bd34539e709e826e96f9a4
RyotaFukumoto <k014c1106@it-neec.jp>
2017-01-16 10:43:37 +0900

    Fixed. #24の課題が完了しました。

---
+++ ex2-1-19/ex2-1-19/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  ex2-1-19
+//
+//  Created by ryota on 2017/01/14.
+//  Copyright © 2017年 ryota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController{
+    NSString *_arguments;
+}
+@property (nonatomic) NSString *arguments;
+@end
