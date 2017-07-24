998d71c85de383480b2dbc35a4f6078b13a18c2b
RyotaFukumoto <k014c1106@it-neec.jp>
2017-07-24 13:55:02 +0900

    RyotaFukumoto#Fixed. #41の課題が完了しました。

---
+++ ex2-5-2/ex2-5-2/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  ex2-5-2
+//
+//  Created by ryota on 2017/07/24.
+//  Copyright © 2017年 ryota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController<UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+
+@end
