1c26841cb3e20609799c2f18e4691a1a11823648
RyotaFukumoto <k014c1106@it-neec.jp>
2017-07-24 11:05:59 +0900

    RyotaFukumoto#Fixed. #40の課題が完了しました。

---
+++ ex2-5-1/ex2-5-1/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  ex2-5-1
+//
+//  Created by ryota on 2017/07/24.
+//  Copyright © 2017年 ryota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MessageUI/MessageUI.h>
+#import <MessageUI/MFMailComposeViewController.h>
+
+@interface ViewController : UIViewController<MFMailComposeViewControllerDelegate>
+
+
+@end
+
