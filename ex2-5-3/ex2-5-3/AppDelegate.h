71717d03006003e6fd7c45beb43cbf7f9268b718
RyotaFukumoto <k014c1106@it-neec.jp>
2017-07-25 13:49:13 +0900

    RyotaFukumoto#Fixed. #42の課題が完了しました。

---
+++ ex2-5-3/ex2-5-3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ex2-5-3
+//
+//  Created by ryota on 2017/07/24.
+//  Copyright © 2017年 ryota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
