fe32af8b47cf380863451884eaf9745c8faf6310
RyotaFukumoto <k014c1106@it-neec.jp>
2017-07-29 00:05:43 +0900

    RyotaFukumoto#Fixed. #49の課題が完了しました。

---
+++ ex2-6-1/ex2-6-1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ex2-6-1
+//
+//  Created by ryota on 2017/07/28.
+//  Copyright © 2017年 ryota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
