21e272d38c03386b9d3dd8ae3b82f3ca03686477
RyotaFukumoto <k014c1106@it-neec.jp>
2017-07-29 16:38:50 +0900

    RyotaFukumoto#Fixed. #50の課題が完了しました。

---
+++ ex2-6-2/ex2-6-2/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ex2-6-2
+//
+//  Created by ryota on 2017/07/29.
+//  Copyright © 2017年 ryota. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
