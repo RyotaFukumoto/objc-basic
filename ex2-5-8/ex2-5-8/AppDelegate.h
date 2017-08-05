b8458438701bf9b3e85523a9d903ee2cb75ce90a
RyotaFukumoto <k014c1106@it-neec.jp>
2017-08-05 16:24:00 +0900

    RyotaFukumoto#Fixed. #47の課題が完了しました。

---
+++ ex2-5-8/ex2-5-8/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ex2-5-8
+//
+//  Created by ryota on 2017/08/02.
+//  Copyright © 2017年 ryota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Firebase.h"
+#import <FirebaseMessaging/FirebaseMessaging.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
